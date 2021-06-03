ab82847fc2f886548207e923b923fad4e6bd409a
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-06-03 13:10:23 +0530

    error handling

--- src/parse.rs
+++ src/parse.rs
@@ -1,21 +1,22 @@
 use std::io::Write;
 
-// only for whole numbers
-
 fn main() {
   let number = match std::env::args().nth(1) {
     Some(n) => n,
     None => {
       std::io::stderr()
-        .write(b"Please supply a number to parse")
+        .write(b"Please supply an integer to parse")
         .unwrap();
       return;
     }
   };
-  println!("{}", string_to_integer(&number));
+  match string_to_integer(&number) {
+    Ok(v) => println!("Successfully parsed {}", v),
+    Err(s) => println!("Error: {}", s),
+  }
 }
 
-fn string_to_integer(s: &str) -> i32 {
+fn string_to_integer(s: &str) -> Result<i32, &'static str> {
   let value = s.trim();
   let is_negative: bool;
   if value.chars().nth(0).unwrap() == '-' {
@@ -25,28 +26,35 @@ fn string_to_integer(s: &str) -> i32 {
   }
   let mut places = value.len() as u32;
   let mut number = 0;
+  let mut v;
   for i in value.chars() {
-    number += mapping(i) * 10_i32.pow(places - 1);
+    v = match mapping(i) {
+      Ok(n) => n,
+      Err(s) => {
+        return Err(s);
+      }
+    };
+    number += v * 10_i32.pow(places - 1);
     places -= 1;
   }
   if is_negative {
-    return -1 * number;
+    return Ok(-1 * number);
   }
-  return number;
+  return Ok(number);
 }
 
-fn mapping(c: char) -> i32 {
+fn mapping(c: char) -> Result<i32, &'static str> {
   return match c {
-    '0' => 0,
-    '1' => 1,
-    '2' => 2,
-    '3' => 3,
-    '4' => 4,
-    '5' => 5,
-    '6' => 6,
-    '7' => 7,
-    '8' => 8,
-    '9' => 9,
-    _ => 0,
+    '0' => Ok(0),
+    '1' => Ok(1),
+    '2' => Ok(2),
+    '3' => Ok(3),
+    '4' => Ok(4),
+    '5' => Ok(5),
+    '6' => Ok(6),
+    '7' => Ok(7),
+    '8' => Ok(8),
+    '9' => Ok(9),
+    _ => Err("Only characters from 0 - 9 allowed"),
   };
 }
